9f4775ed89d745ea07bf0c8e69dfefa40b035e94
Puja Vijay Suralkar <pujavijaysuralkar@gmail.com>
2026-06-27 23:48:28 +0530

    second approach of Moving -ve no To begining

---
+++ src/Array/ntiveToBegPtiveToEnd2Appr.java
@@ -0,0 +1,36 @@
+package Array;
+
+public class ntiveToBegPtiveToEnd2Appr {
+    public static int[] move(int[] arr){
+        int left=0;
+        int right=arr.length-1;
+
+        while(left<right){
+            while(left<right && arr[left]<0){
+                left++;
+            }
+            while(right>left && arr[right]>0){
+                right--;
+            }
+
+            if(right>left){
+                int temp=arr[left];
+                arr[left]=arr[right];
+                arr[right]=arr[left];
+                left++;
+                right--;
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={-15, 11, -13, -5, 6, -7, 5, -3, -6};
+         int[] ans=move(arr);
+
+         for(int num: ans){
+             System.out.print(num);
+         }
+        System.out.println();
+    }
+}
